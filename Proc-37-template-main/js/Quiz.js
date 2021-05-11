91617502bd6c763497625db01fa76a05a72f7a97
Simon5155Q <75984605+Simon5155Q@users.noreply.github.com>
2021-05-11 23:27:25 +0530

    Add files via upload

---
+++ Proc-37-template-main/js/Quiz.js
@@ -0,0 +1,57 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+      
+    
+
+    
+
+   getContestantInfo()
+
+
+    if(allContestants !== undefined){
+      var displayPos = 150;
+    for(var cnts in allContestants){
+      if(answer === 2){
+        fill("green");
+      }
+      else{
+        fill("red");
+      }
+      displayPos += 20;
+      text(allContestants[cnts].name + ":" + allContestants[cnts], 120, displayPos);
+    }
+   
+    
+  }
+
+  }
+}
